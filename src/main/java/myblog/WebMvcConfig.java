9bf844abd1a36cc33bf7aaa3e989cbc8efa096b2
yk1028 <wongeunsong94@gmail.com>
2019-09-30 18:23:03 +0900

    feat: 모든 정적 자원에 대해 no-cache, no-store 설정을 한다.
    - intercepterRegitry를 활용하면 가능하다. 대신 addResourceHandlers로 등록한 설정이 적용되지 않는다. 뭔가 정상적인 방법은 아닌것 같다.

--- src/main/java/myblog/WebMvcConfig.java
+++ src/main/java/myblog/WebMvcConfig.java
@@ -6,11 +6,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.CacheControl;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 import support.version.BlogVersion;
 
 import javax.servlet.Filter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.concurrent.TimeUnit;
 
 @Configuration
@@ -22,18 +27,31 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler(PREFIX_STATIC_RESOURCES + "/" + version.getVersion() + "/css/**")
-                .addResourceLocations("classpath:/static/css/")
-                .setCacheControl(CacheControl.maxAge(31536000, TimeUnit.MILLISECONDS));
-
-
-        registry.addResourceHandler(PREFIX_STATIC_RESOURCES + "/" + version.getVersion() + "/js/**")
-                .addResourceLocations("classpath:/static/js/")
-                .setCacheControl(CacheControl.noCache().cachePrivate());
+//        registry.addResourceHandler(PREFIX_STATIC_RESOURCES + "/" + version.getVersion() + "/css/**")
+//                .addResourceLocations("classpath:/static/css/")
+//                .setCacheControl(CacheControl.maxAge(31536000, TimeUnit.MILLISECONDS));
+//
+//
+//        registry.addResourceHandler(PREFIX_STATIC_RESOURCES + "/" + version.getVersion() + "/js/**")
+//                .addResourceLocations("classpath:/static/js/")
+//                .setCacheControl(CacheControl.noCache().cachePrivate());
 //
         registry.addResourceHandler(PREFIX_STATIC_RESOURCES + "/" + version.getVersion() + "/**")
                 .addResourceLocations("classpath:/static/")
                 .setCacheControl(CacheControl.noCache().cachePrivate());
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new HandlerInterceptorAdapter() {
+
+            @Override
+            public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+                response.setHeader("Cache-Control","no-cache, no-store");
+                response.setHeader("Pragma","no-cache");
+                response.setDateHeader("Expires",0);
+            }
+        });
     }
 
     @Bean
